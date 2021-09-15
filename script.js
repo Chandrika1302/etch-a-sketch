2a510b1b348a15852ca97014058d724de309a6b3
Chandrika <kummarakuntla.11901601@lpu.in>
2021-09-15 10:23:33 +0530

    added reset function,css and updated code

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-const gridContainer=document.querySelector('#container');
-for(let i=0;i<25;i++){
-    const gridItems=document.createElement('div');
-    gridItems.classList.add('div-color');
-    gridContainer.append(gridItems);
+const gridContainer = document.querySelector("#container");
+const userValue = document.querySelector("#form");
+const reset = document.querySelector("#reset-btn");
+// userValue.addEventListener('submit',main);
+// function main(e){
+//     e.preventDefault();
+//     const input1=e.target.inputname1;
+//     console.log(input1.value);
+    
+// }
+reset.addEventListener('click',resetBtn);
+
+let gridCreation=function(){
+    for (let i = 0; i < 25; i++) {
+        const gridItem = document.createElement("div");
+        gridItem.addEventListener("mouseenter", function (event) {
+          event.target.classList.add("darken");
+        });
+        gridContainer.append(gridItem);
+      }
 }
-const gridColor=[...document.querySelectorAll('div.div-color')];
-gridColor.forEach(item=>item.addEventListener("mouseenter",function(event){
-    event.target.style.backgroundColor="red";
-}))
+gridCreation();
+
+function resetBtn(){
+    gridContainer.innerHTML='';
+    gridCreation();
+}
 
