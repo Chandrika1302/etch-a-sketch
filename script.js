151b3bc0da1f3b8d5d827d747ca23c1ae5a5abed
Chandrika <kummarakuntla.11901601@lpu.in>
2021-09-16 17:49:02 +0530

    updated all the logics

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 const gridContainer = document.querySelector("#container");
 const userValue = document.querySelector("#form");
 const reset = document.querySelector("#reset-btn");
-// userValue.addEventListener('submit',main);
-// function main(e){
-//     e.preventDefault();
-//     const input1=e.target.inputname1;
-//     console.log(input1.value);
+userValue.addEventListener('submit',main);
+function main(e){
+    e.preventDefault();
+    const input1=e.target.inputname1.value;
+    gridCreation(input1);
     
-// }
+}
+
 reset.addEventListener('click',resetBtn);
 
-let gridCreation=function(){
-    for (let i = 0; i < 25; i++) {
+let gridCreation=function(x){
+    gridContainer.style.gridTemplateColumns=`repeat(${x},1fr)`;
+    for (let i = 0; i < x*x; i++) {
         const gridItem = document.createElement("div");
         gridItem.addEventListener("mouseenter", function (event) {
           event.target.classList.add("darken");
