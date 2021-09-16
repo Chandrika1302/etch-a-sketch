f36f47ac99d83e93a8c76abde4a96111bdb079bf
Chandrika <kummarakuntla.11901601@lpu.in>
2021-09-16 21:48:01 +0530

    added css,logics

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
 const gridContainer = document.querySelector("#container");
 const userValue = document.querySelector("#form");
 const reset = document.querySelector("#reset-btn");
-userValue.addEventListener('submit',main);
-function main(e){
-    e.preventDefault();
-    const input1=e.target.inputname1.value;
-    gridTemplateCreation(input1);
-    
-}
 
-reset.addEventListener('click',resetBtn);
-
-function gridTemplateCreation(x){
-    gridContainer.style.gridTemplateColumns=`repeat(${x},1fr)`;
-    for (let i = 0; i < x*x; i++) {
-        const gridItem = document.createElement("div");
-        gridItem.addEventListener("mouseenter", function (event) {
-          event.target.classList.add("darken");
-        });
-        gridContainer.append(gridItem);
-      }
-}
 gridTemplateCreation(5);
 
-function resetBtn(){
-    gridContainer.innerHTML='';
-    gridTemplateCreation(5);
-}
+reset.addEventListener("click", resetBtn);
+userValue.addEventListener("submit", main);
 
-//check it now
+function main(e) {
+  e.preventDefault();
+  const input1 = e.target.inputname1.value;
+  gridTemplateCreation(input1);
+}
+function gridTemplateCreation(x) {
+  gridContainer.style.gridTemplateColumns = `repeat(${x},1fr)`;
+  gridContainer.innerHTML = "";
+  for (let i = 0; i < x * x; i++) {
+    const gridItem = document.createElement("div");
+    gridItem.addEventListener("mouseenter", function (event) {
+      event.target.classList.add("darken");
+    });
+    gridContainer.append(gridItem);
+  }
+}
+function resetBtn() {
+  gridTemplateCreation(5);
+}
